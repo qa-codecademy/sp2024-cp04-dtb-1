6ee2f964b0f035182e54806842e9f775a2703135
Bojan Jankov <jankovbojan4@gmail.com>
2024-09-07 14:05:58 +0200

    finished register form page

--- client/src/app/feature/auth/components/register/register.component.ts
+++ client/src/app/feature/auth/components/register/register.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  registerForm = this.generateRegisterForm();
 
+  generateRegisterForm() {
+    return new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', Validators.required),
+    });
+  }
+
+  onFormSubmit() {
+    this.registerForm.markAllAsTouched();
+
+    if (this.registerForm.invalid) return;
+
+    console.log(this.registerForm.value);
+  }
 }
