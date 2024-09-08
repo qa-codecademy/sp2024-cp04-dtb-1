d4cbf071cbafe65bd9dc37c32cb259608cf9b6ba
Bojan Jankov <jankovbojan4@gmail.com>
2024-09-08 11:59:30 +0200

    added scroll to top button, and change some css

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/components/header/header.component';
 import { FooterComponent } from './core/components/footer/footer.component';
+import { ScrollToTopComponent } from './shared/scroll-to-top/scroll-to-top.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    ScrollToTopComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
